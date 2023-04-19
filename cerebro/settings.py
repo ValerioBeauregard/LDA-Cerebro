e66dc97aecdff9c29174e4f33c103842c26714fa
ValerioBeauregard <valeriobeauregard@hotmail.com>
2023-04-19 11:34:30 -0600

    Adding static files folder

--- cerebro/settings.py
+++ cerebro/settings.py
@@ -143,6 +143,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
         os.path.join(BASE_DIR, 'static'),
     )
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
